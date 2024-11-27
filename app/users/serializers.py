a789e849acb7bea730b7fb78bbfbbf49a74ac860
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-11-28 07:38:25 +0800

    Make group_members read_only

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -2,21 +2,28 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from .models import Users, CapstoneGroups, UserProfile
 
-class CSVFileSerializer(serializers.Serializer): # For bulk creation of users throgh upload
+
+class CSVFileSerializer(
+    serializers.Serializer
+):  # For bulk creation of users throgh upload
     file = serializers.FileField()
+
 
 class EmailAndPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
+
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
+
 
 class ResetPasswordSerializer(serializers.Serializer):
     token = serializers.CharField()
     new_password = serializers.CharField()
     confirm_password = serializers.CharField()
-    
+
+
 class ChangeCurrentPasswordSerializer(serializers.Serializer):
     current_password = serializers.CharField()
     new_password = serializers.CharField()
@@ -27,7 +34,8 @@ class UsersCustomSerializer(serializers.Serializer):
     email = serializers.EmailField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
-    
+
+
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
@@ -42,15 +50,26 @@ class UsersSerializer(serializers.ModelSerializer):
             self.fields.pop("email", None)
             self.fields.pop("password", None)
 
+
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = "__all__"
         depth = 1
-        
+
+
 class CapstoneGroupsSerializer(serializers.ModelSerializer):
-    group_members = UsersCustomSerializer(many=True)    
-    
+    group_members = UsersCustomSerializer(many=True, read_only=True)
+
     class Meta:
         model = CapstoneGroups
-        fields = ["id", "created_at", "updated_at", "name", "academic_year", "course", "specialization", "group_members"]
+        fields = [
+            "id",
+            "created_at",
+            "updated_at",
+            "name",
+            "academic_year",
+            "course",
+            "specialization",
+            "group_members",
+        ]
