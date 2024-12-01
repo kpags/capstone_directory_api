d2582e7df2303589c2d63766cb228f007eac7960
Kurt Paguio <kurt.paguio.simpletrade@gmail.com>
2024-12-01 19:18:41 +0800

    Modified permissions and filters for capstone projects

--- app/capstone_projects/filters.py
+++ app/capstone_projects/filters.py
@@ -6,7 +6,6 @@ class CapstoneProjectsFilter(filters.FilterSet):
     is_best_project = filters.BooleanFilter(field_name="is_best_project")
     is_approved = filters.BooleanFilter(field_name="is_approved")
     status = filters.CharFilter(field_name="status", lookup_expr="iexact")
-    keywords = filters.CharFilter(method="filter_by_keywords")
     sort_by = filters.CharFilter(method="filter_by_sort_type")
     is_ip_registered = filters.CharFilter(method="filter_by_is_ip_registered")
     search = filters.CharFilter(method="filter_by_search")
@@ -29,11 +28,6 @@ class CapstoneProjectsFilter(filters.FilterSet):
             Q(keywords__overlap=array_values)
         )
             
-    def filter_by_keywords(self, queryset, name, value):
-        array_values = value.lower().split(" ")
-        
-        return queryset.filter(keywords__overlap=array_values)
-    
     def filter_by_sort_type(self, queryset, name, value):
         if value.lower() == "newest":
             return queryset.order_by('-created_at')
